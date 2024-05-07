cdebcc5486d9f1da97680293e39766a2d0fee2b2
Konrad Konkel <konrad.konkel@wp.pl>
2024-05-07 13:07:04 +0100

    programmatically rendering 16x16 grid using flexbox

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 const container = document.querySelector("#container");
-console.log(container);
+
+for (let i = 0 ; i < 16 ; i++) {
+    const divColumn = document.createElement("div");
+    divColumn.classList.add("divColumn");
+    divColumn.setAttribute("id", `divColumn${i}`);
+
+    for (let j = 0 ; j < 16 ; j++) {
+        const squareDiv = document.createElement("div");
+        squareDiv.classList.add("squareDiv");
+        squareDiv.setAttribute("id", `squareDiv${j}`);
+        divColumn.appendChild(squareDiv);
+    }
+    
+    container.appendChild(divColumn);
+}
