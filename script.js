8fb604cb95294df5a6b1d4972875893853307182
Konrad Konkel <konrad.konkel@wp.pl>
2024-05-07 14:39:12 +0100

    grid is redrawn, but takes to much or to little space

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
+const MAX_WIDTH = 1120;
+const MAX_HEIGHT = 1120;
+const INITIAL_GRID_SIZE = 16;
+
+let width = MAX_WIDTH;
+let height = MAX_HEIGHT;
+let gridSize = INITIAL_GRID_SIZE;
+
 const container = document.querySelector("#container");
 
-for (let i = 0 ; i < 16 ; i++) {
-    const divColumn = document.createElement("div");
-    divColumn.classList.add("divColumn");
-    divColumn.setAttribute("id", `divColumn${i}`);
+const drawGrid = () => {
+    const squareSideSize = Math.floor(MAX_HEIGHT / gridSize);
 
-    for (let j = 0 ; j < 16 ; j++) {
-        const squareDiv = document.createElement("div");
-        squareDiv.classList.add("squareDiv");
-        squareDiv.setAttribute("id", `squareDiv${j}`);
-        divColumn.appendChild(squareDiv);
-    }
-    
-    container.appendChild(divColumn);
+    for (let i = 0 ; i < gridSize ; i++) {
+        const divColumn = document.createElement("div");
+        divColumn.classList.add("divColumn");
+        divColumn.setAttribute("id", `divColumn${i}`);
+
+        for (let j = 0 ; j < gridSize ; j++) {
+            const squareDiv = document.createElement("div");
+            squareDiv.classList.add("squareDiv");
+            squareDiv.setAttribute("id", `squareDiv${j}`);
+            squareDiv.style.width = `${squareSideSize}px`;
+            squareDiv.style.height = `${squareSideSize}px`;
+            squareDiv.addEventListener("mouseover", () => {
+                squareDiv.style.backgroundColor = "black";
+            })
+            divColumn.appendChild(squareDiv);
+        }
+
+        container.appendChild(divColumn);
+    }   
 }
+
+drawGrid();
+
+const button = document.querySelector("button");
+let result = button.addEventListener("click", () => {
+    let girdChoice = prompt("Type in grid side size", 10);
+    if (girdChoice > 100 || girdChoice <= 0) {
+        alert("Grid side must be between 1-100");
+        return;
+    }
+    gridSize = girdChoice;
+    container.replaceChildren();
+    drawGrid();
+})
+
